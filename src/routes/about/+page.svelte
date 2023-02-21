91bf3b11a44ef419f88925137a70d6d75e7216b2
Cat <git@frcat.xyz>
2023-02-20 23:27:40 -0500

    + Upd

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+	import Header from './Header.svelte'
+</script>
+
+<svelte:head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<title>Andrew's World!</title>
+
+	<meta property="og:title" content="Andrew's World" />
+	<meta property="og:site_name" content="penis.today" />
+	<meta property="og:url" content="https://penis.today" />
+	<meta property="og:description" content="AIOA (All in one Andrew)" />
+	<meta property="og:type" content="website" />
+	<meta
+		property="og:image"
+		content="https://cdn.discordapp.com/avatars/355091763825868800/a_aede142fc5586b648e038486987f4090.gif?size=1024"
+	/>
+</svelte:head>
+
+<Header></Header>
+
+<main>
+	Hello! I'm Andrew, a really super cool epic gamer.
+	<br />
+	I'm currently in a networking class and that’s a cool fact I guess.
+	<br />
+	Click on the links at the top to expore the rest of my website!
+</main>
